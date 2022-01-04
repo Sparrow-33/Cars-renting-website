7bf5c918e732a848a033bc686753d4033adb0b5f
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-01-04 20:36:12 +0100

    style JS added

--- contact.js
+++ contact.js
@@ -1,20 +1,20 @@
 
-let 
-
-
-
+// import swal from 'sweetalert'; 
 
 function popup() {
     
-    let name = document.querySelector(".name"),
-     lastName = document.querySelector(".lastName"),
-      email = document.querySelector(".Email"),
+    // let name = document.querySelector(".name"),
+    //  lastName = document.querySelector(".lastName"),
+    //   email = document.querySelector(".Email"),
       message = document.querySelector(".message");
 
 
-
-
-    window.alert("First Name :  "+name.value+ "\nLast Name :"
-                                +lastName.value+"\nEmail :"
-                                +email.value+"\nMessage :"+message.value );
+    swal({
+      title: "Message Submited!",
+      text: message.innerText,
+      icon: "success",
+      button: "Aww yiss!",
+    });   // swal("First Name :  "+name.value+ "\nLast Name :"
+    //                             +lastName.value+"\nEmail :"
+    //                             +email.value+"\nMessage :"+message.value,"");
 }
